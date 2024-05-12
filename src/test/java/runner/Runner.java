48c59ff234a224d2b650c7ce6e478fd7243da50d
Elif Atci <eelif0600@gmail.com>
2024-05-12 18:30:19 +0300

    Elif/10.05/changes have been added

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -5,7 +5,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty",
-                "json:target/cucumber.json",},
+                "html:target/cucumberHooks.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "@us17" ,
